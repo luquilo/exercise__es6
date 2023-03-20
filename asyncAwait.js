2f03cb0cfcd786b4583e9919ba5f59cc042c3dd6
luquilo <luquilo.etc@gmail.com>
2023-03-20 20:29:01 +0700

    handling dependent async await stuff

--- asyncAwait.js
+++ asyncAwait.js
@@ -1,52 +1,114 @@
-// // async function coba (){
-// //     console.log('satu')
-// //     setTimeout(() => console.log('halo'),5000)
+// // // // async function coba (){
+// // // //     console.log('satu')
+// // // //     setTimeout(() => console.log('halo'),5000)
+// // // // }
+
+// // // // coba()
+
+// // // // async function numberFive() {
+// // // //   return 5;
+// // // // }
+
+// // // // numberFive().then((resolvedValue) => console.log(resolvedValue));
+
+// // // const wait = () => {
+// // //   return new Promise((resolve, reject) => {
+// // //     setTimeout(() => {
+// // //         console.log('tiga')
+// // //         resolve()
+// // //     } ,3000)
+// // //   });
+// // // };
+
+// // // const coba = async () => {
+// // //   await wait();
+// // //   console.log("halo 5");
+// // // };
+
+// // // coba();
+
+// // const coba = async (MS) => {
+// //     return new Promise((resolve, reject) => {
+// //         setTimeout(
+// //             () => {
+// //                 console.log(`the ms is : ${MS}`)
+// //                 resolve()
+// //             },
+// //             MS
+// //         )
+// //     })
 // // }
 
-// // coba()
-
-// // async function numberFive() {
-// //   return 5;
+// // const anu = async () => {
+// //     const randomMS = Math.floor(Math.random() *5 * 1000)
+// //     console.log(randomMS)
+// //     await coba(randomMS)
+// //     console.log('done')
 // // }
 
-// // numberFive().then((resolvedValue) => console.log(resolvedValue));
+// // anu()
 
-// const wait = () => {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//         console.log('tiga')
-//         resolve()
-//     } ,3000)
-//   });
-// };
+// const myPromise = () => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve('resolved!')
+//         },
+//         1500)
+//     })
+// }
 
-// const coba = async () => {
-//   await wait();
-//   console.log("halo 5");
-// };
+// const yesAwait = async () => {
+//     console.log('hold on a second!')
+//     const data = await myPromise()
+//     console.log(data)
+// }
+// yesAwait()
 
-// coba();
+/*
+This is the shopForBeans function from the last exercise
+*/
 
+const shopForBeans = () => {
+  return new Promise((resolve, reject) => {
+    const beanTypes = ["kidney", "fava", "pinto", "black", "garbanzo"];
+    setTimeout(() => {
+      let randomIndex = Math.floor(Math.random() * 5);
+      let beanType = beanTypes[randomIndex];
+      console.log(`I bought ${beanType} beans because they were on sale.`);
+      resolve(beanType);
+    }, 1000);
+  });
+};
 
+let soakTheBeans = (beanType) => {
+  return new Promise((resolve, reject) => {
+    console.log("Time to soak the beans.");
+    setTimeout(() => {
+      console.log(`... The ${beanType} beans are softened.`);
+      resolve(true);
+    }, 1000);
+  });
+};
 
-const coba = async (MS) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(
-            () => {
-                console.log(`the ms is : ${MS}`)
-                resolve()
-            },
-            MS
-        )
-    })
+let cookTheBeans = (isSoftened) => {
+  return new Promise((resolve, reject) => {
+    console.log("Time to cook the beans.");
+    setTimeout(() => {
+      if (isSoftened) {
+        console.log("... The beans are cooked!");
+        resolve("\n\nDinner is served!");
+      }
+    }, 1000);
+  });
+};
+
+const makeBeans = async () => {
+    let buy = await shopForBeans()
+
+    let soak = await soakTheBeans(buy)
+
+    let cook  = await cookTheBeans(soak)
+    console.log(cook)
 }
 
-
-const anu = async () => {
-    const randomMS = Math.floor(Math.random() *5 * 1000)
-    console.log(randomMS)
-    await coba(randomMS)
-    console.log('done')
-}
-
-anu()
+makeBeans()
