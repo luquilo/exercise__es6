385706bfe199686e24d175a1b0ab916854b33267
luquilo <luquilo.etc@gmail.com>
2023-03-22 21:39:30 +0700

    belajar custom element dari dicoding

---
+++ dicoding_custom_element/customElement.js
@@ -0,0 +1,8 @@
+class ImageFigure extends HTMLElement {
+
+}
+
+// custom elements merupakan object global yang digunakan untuk 
+// => mendefinisikan custom element dan memberi tahu bahwa ada html tag baru
+customElements.define('image-figure', ImageFigure)
+
