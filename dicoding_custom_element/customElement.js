ed4f2a207543a8883ea5abb5467fda95ea6e522f
luquilo <luquilo.etc@gmail.com>
2023-03-22 23:27:18 +0700

    membuat custom element

--- dicoding_custom_element/customElement.js
+++ dicoding_custom_element/customElement.js
@@ -1,8 +1,40 @@
 class ImageFigure extends HTMLElement {
+  constructor() {
+    super();
+    console.log("constructed!");
+  }
 
+  connectedCallback() {
+    this.src = this.getAttribute("src") || null
+    this.alt = this.getAttribute("alt") || null
+    this.caption = this.getAttribute('caption') || null
+
+    this.innerHTML = `
+        <figure>
+          <img src='${this.src}' alt='${this.alt}'>
+          <figcaption>${this.caption}</figcaption>
+        </figure>
+    `
+
+  }
+
+  disconnectedCallback() {
+    console.log("disconnected!");
+  }
+
+  adoptepCallback() {
+    console.log("adoptep!");
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log(`Attribute: ${name} changed!`);
+  }
+
+  static get observedAttribute() {
+    return ["caption"];
+  }
 }
 
-// custom elements merupakan object global yang digunakan untuk 
+// custom elements merupakan object global yang digunakan untuk
 // => mendefinisikan custom element dan memberi tahu bahwa ada html tag baru
-customElements.define('image-figure', ImageFigure)
-
+customElements.define("image-figure", ImageFigure);
