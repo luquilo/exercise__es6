63704034678171893c629e9bbd4d11b3d67785f0
luquilo <luquilo.etc@gmail.com>
2023-03-23 10:50:46 +0700

    belajar mengubah attribute dan re render element html

--- dicoding_custom_element/customElement.js
+++ dicoding_custom_element/customElement.js
@@ -1,36 +1,32 @@
 class ImageFigure extends HTMLElement {
-  constructor() {
-    super();
-    console.log("constructed!");
-  }
+  // constructor() {
+  //   super();
+  //   console.log("constructed!");
+  // }
 
   connectedCallback() {
     this.src = this.getAttribute("src") || null
     this.alt = this.getAttribute("alt") || null
     this.caption = this.getAttribute('caption') || null
+    this.class = this.getAttribute('class') || null
+    this.render()
+  }
 
+  render() {
     this.innerHTML = `
-        <figure>
+        <figure class='${this.class}'>
           <img src='${this.src}' alt='${this.alt}'>
           <figcaption>${this.caption}</figcaption>
         </figure>
     `
-
-  }
-
-  disconnectedCallback() {
-    console.log("disconnected!");
-  }
-
-  adoptepCallback() {
-    console.log("adoptep!");
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
-    console.log(`Attribute: ${name} changed!`);
+    this[name] = newValue;
+    this.render()
   }
 
-  static get observedAttribute() {
+  static get observedAttributes() {
     return ["caption"];
   }
 }
