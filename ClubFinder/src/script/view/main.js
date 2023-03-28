18d250e0416a0378e666fc0bbd8c2c5ad8ee7fdd
luquilo <luquilo.etc@gmail.com>
2023-03-28 11:36:37 +0700

    membuat custom component club-list dan club-item

--- ClubFinder/src/script/view/main.js
+++ ClubFinder/src/script/view/main.js
@@ -1,44 +1,26 @@
 import DataSource from "../data/data-source.js";
 import '../component/search-bar.js'
-
+import '../component/club-list.js'
 
 const main = () => {
   const searchElement = document.querySelector('search-bar');
-  const clubListElement = document.querySelector('#clubList');
+  const clubListElement = document.querySelector('club-list');
 
-  const onButtonSearchClicked = () => {
-    // const dataSource = new DataSource(renderResult, fallbackResult);
-    
-    DataSource.searchClub(searchElement.value)
-      .then(renderResult)
-      .catch(fallbackResult)
-    
-    // dataSource.searchClub(searchElement.value);
+  const onButtonSearchClicked = async () => {
+    try {
+      const result = await DataSource.searchClub(searchElement.value);
+      renderResult(result);
+    } catch (message) {
+      fallbackResult(message);
+    }
   };
 
   const renderResult = results => {
-    clubListElement.innerHTML = '';
-    results.forEach(function (club) {
-      const {name, fanArt, description} = club
-
-      const clubElement = document.createElement('div');
-      clubElement.setAttribute('class', 'club');
-
-      clubElement.innerHTML = 
-      `
-          <img class='fan-art-club' src='${fanArt}' alt='Fan Art'>
-          <div class='club-info'>
-          <h2>${name}</h2>
-          <p>${description}</p>
-          </div>
-      `
-      clubListElement.appendChild(clubElement);
-    });
+    clubListElement.clubs = results    
   };
 
   const fallbackResult = message => {
-    clubListElement.innerHTML = '';
-    clubListElement.innerHTML += `<h2 class='placeholder'>${message}</h2>`
+    clubListElement.renderError(message)
   };
 
   searchElement.clickEvent = onButtonSearchClicked;
