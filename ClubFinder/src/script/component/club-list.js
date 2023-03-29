b667f0eb81c4d6c240a20e6a0f976f51bde98a23
luquilo <luquilo.etc@gmail.com>
2023-03-29 07:19:48 +0700

    berlatih shadow dom

--- ClubFinder/src/script/component/club-list.js
+++ ClubFinder/src/script/component/club-list.js
@@ -1,12 +1,36 @@
 import './club-item.js'
 
 class ClubList extends HTMLElement {
+
+  constructor(){
+    super();
+    this.shadowDOM = this.attachShadow({mode:'open'})
+  }
+
   set clubs(clubs) {
     this._clubs = clubs;
     this.render();
   }
+
+  renderError(message) {
+    this.shadowDOM.innerHTML = 
+    `
+      <style>
+      .placeholder {
+        font-weight: lighter;
+        color: rgba(0, 0, 0, 0.5);
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+      </style>
+    `
+    this.shadowDOM.innerHTML += `<h2 class='placeholder'>${message}</h2>`;
+  }
+
   render() {
-    this.innerHTML = "";
+    this.shadowDOM.innerHTML = "";
     this._clubs.forEach((club) => {
       const clubItemElement = document.createElement("club-item");
       clubItemElement.club = club;
@@ -14,10 +38,6 @@ class ClubList extends HTMLElement {
     });
   }
 
-  renderError(message) {
-    this.innerHTML = "";
-    this.innerHTML += `<h2 class='placeholder'>${message}</h2>`;
-  }
 }
 
 customElements.define('club-list', ClubList);
